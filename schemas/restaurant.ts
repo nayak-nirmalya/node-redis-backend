6b548705b93a82879a66f0f3eda32ed19564476b
Nirmalya Nayak <n.nayak263@gmail.com>
2024-12-06 19:29:40 +0530

    add schema: RestaurantDetailsSchema

--- schemas/restaurant.ts
+++ schemas/restaurant.ts
@@ -5,3 +5,19 @@ export const RestaurantSchema = z.object({
   location: z.string().min(1),
   cuisines: z.array(z.string().min(1)),
 });
+
+export const RestaurantDetailsSchema = z.object({
+  links: z.array(
+    z.object({
+      name: z.string().min(1),
+      url: z.string().min(1),
+    })
+  ),
+  contact: z.object({
+    phone: z.string().min(1),
+    email: z.string().email(),
+  }),
+});
+
+export type Restaurant = z.infer<typeof RestaurantSchema>;
+export type RestaurantDetails = z.infer<typeof RestaurantDetailsSchema>;
