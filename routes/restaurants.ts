ac019c8d60b87f6029f4e701bf5bae611a0a54ee
Nirmalya Nayak <n.nayak263@gmail.com>
2024-12-10 18:54:59 +0530

    downgrade @types/express version

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
+import { validate } from "../middlewares/validate.js";
+import { RestaurantSchema } from "../schemas/restaurant.js";
+
 export const router = express.Router();
 
 router.get("/", async (req, res) => {
   res.send("Hello, World!");
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validate(RestaurantSchema), async (req, res) => {
   const data = req.body;
 });
