5cb34227671eb1a3a3bd2276aa43ae102cd909a1
Nirmalya Nayak <n.nayak263@gmail.com>
2024-12-16 17:19:43 +0530

    add fn: cuisinesKey & cuisineKey

--- utils/keys.ts
+++ utils/keys.ts
@@ -6,3 +6,5 @@ export const restaurantKeyById = (id: string) => getKeyName("restaurants", id);
 export const reviewKeyById = (id: string) => getKeyName("reviews", id);
 export const reviewDetailsKeyById = (id: string) =>
   getKeyName("review_details", id);
+export const cuisinesKey = getKeyName("cuisines");
+export const cuisineKey = (name: string) => getKeyName("cuisine", name);
