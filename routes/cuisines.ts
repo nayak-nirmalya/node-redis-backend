af23fffc028692b31883fbe7925f78b8af631069
Nirmalya Nayak <n.nayak263@gmail.com>
2024-12-17 07:10:36 +0530

    add route: GET /cuisines to get list of all unique cuisines

--- routes/cuisines.ts
+++ routes/cuisines.ts
@@ -1,3 +1,18 @@
 import express from "express";
 
+import { initializeRedisClient } from "../utils/client.js";
+import { cuisinesKey } from "../utils/keys.js";
+import { successResponse } from "../utils/responses.js";
+
 export const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const client = await initializeRedisClient();
+    const cuisines = await client.sMembers(cuisinesKey);
+
+    return successResponse({ res, data: cuisines });
+  } catch (error) {
+    next(error);
+  }
+});
